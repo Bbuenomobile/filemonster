5526f87210f4c83fd42245c145cb4ff56988e8c7
Nikhil <nikhilsunil90s@gmail.com>
2022-09-20 17:12:05 +0530

    New Changes

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require("path");
 const app = express();
 const PORT = 8100;
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.static(path.join(__dirname,"uploads")));
 
 app.get("/" , (req,res,next) => {
@@ -13,6 +14,7 @@ app.get("/" , (req,res,next) => {
 app.listen(PORT, (err) =>{
     if (!err) {
         console.log("Files Serving At :8100");
+        console.log("From - ", path.join(__dirname,"uploads"));
     } else {
         console.log(err);
     }
