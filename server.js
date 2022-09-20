d9e6b74a995b56fd48fa2a271320bb0044640b35
Nikhil <nikhilsunil90s@gmail.com>
2022-09-20 14:53:04 +0530

    Files First

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = 7000;
+
+
+app.use("/uploads" ,express.static("uploads"))
+  
+app.listen(PORT, (err) =>{
+    if (!err) {
+        console.log("Files Serving At :7000");
+    } else {
+        console.log(err);
+    }
+});
