985b794fe43bd51840a7ecab4ce45f15ee1cd6ed
Nikhil <nikhilsunil90s@gmail.com>
2022-09-20 17:20:02 +0530

    New Changes

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const PORT = 8100;
+const PORT = process.env.PORT || 8100;
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.static("uploads"));
 
-app.get("/" , (req,res,next) => {
-    res.send("Serving Files!");
-})
-  
 app.listen(PORT, (err) =>{
     if (!err) {
         console.log("Files Serving At :8100");
